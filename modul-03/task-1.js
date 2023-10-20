ef37ab7c8158fa604fed2596fce7b241103ccdb8
Terrad77 <terlovoy77@gmail.com>
2023-10-20 22:43:28 +0300

    uncomment

--- modul-03/task-1.js
+++ modul-03/task-1.js
@@ -1,13 +1,10 @@
 "use strict";
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = quantity * pricePerDroid;
-  if (customerCredits < totalPrice) {
-    return `"Insufficient funds!"`;
-  }
-  return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+function getLastElementMeta(array) {
+  return [array.length - 1, array[array.length - 1]];
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(getLastElementMeta(["apple", "peach", "pear", "banana"])); // повертає [3, "banana"]);
+
+// const planets = ["Earth", "Mars", "Venus"];
+// const lastElementIndex = planets.length - 1;
+// console.log(planets[lastElementIndex]); // "Venus"
